9be7854a6125e66de5b126fe6f8c2ef657c9f25b
niallmccloy1 <niallmccloy@protonmail.com>
2025-04-19 00:13:04 +0100

    Delete Chore Pop up working

--- lib/ui/chores/view_all_chores/widgets/view_all_chores_screen.dart
+++ lib/ui/chores/view_all_chores/widgets/view_all_chores_screen.dart
@@ -1,4 +1,6 @@
 import 'package:chore_app/ui/chores/view_all_chores/widgets/view_all_chores_table.dart';
+import 'package:chore_app/domain/repositories/chore_repository.dart';
+import 'package:chore_app/domain/models/chore_model.dart';
 import 'package:flutter/material.dart';
 
 class ViewAllChoresScreen extends StatefulWidget {
@@ -11,6 +13,25 @@ class ViewAllChoresScreen extends StatefulWidget {
 }
 
 class _ViewAllChoresScreenState extends State<ViewAllChoresScreen> {
+  final ChoreRepository _choreRepository = ChoreRepository();
+  List<Chore> _chores = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadChores();
+  }
+
+  Future<void> _loadChores() async {
+    _chores = await _choreRepository.getAll();
+    setState(() {});
+  }
+
+  Future<void> _deleteChore(int id) async {
+    await _choreRepository.delete(id);
+    await _loadChores();
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +39,10 @@ class _ViewAllChoresScreenState extends State<ViewAllChoresScreen> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: ViewAllChoresTable(),
+      body: ViewAllChoresTable(
+        chores: _chores,
+        onDelete: _deleteChore,
+      ),
     );
   }
 }
