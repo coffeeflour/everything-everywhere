618fe85ee2452aad56d813e43cab69dddff4832f
coffeeflour <coffeeflour@proton.me>
2025-04-19 01:06:28 +0100

    started work on edit page + table :)

--- lib/ui/chores/view_all_chores/widgets/view_all_chores_table.dart
+++ lib/ui/chores/view_all_chores/widgets/view_all_chores_table.dart
@@ -1,20 +1,23 @@
 import 'package:chore_app/domain/models/chore_model.dart';
+import 'package:chore_app/ui/chores/edit_chore/widgets/edit_chore_screen.dart';
 import 'package:chore_app/ui/core/ui/widgets/delete_chore_popup.dart';
 import 'package:flutter/material.dart';
 
 class ViewAllChoresTable extends StatelessWidget {
   final List<Chore> chores;
   final Future<void> Function(int) onDelete;
+  final Future<void> Function(int) onEdit;
 
   const ViewAllChoresTable({
     Key? key,
     required this.chores,
     required this.onDelete,
+    required this.onEdit,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-   return DataTable(
+    return DataTable(
       columns: const <DataColumn>[
         DataColumn(label: Text('Name')),
         DataColumn(label: Text('Description')),
@@ -22,33 +25,50 @@ class ViewAllChoresTable extends StatelessWidget {
         DataColumn(label: Text('Completed')),
         DataColumn(label: Text('Actions')),
       ],
-      rows: chores.map((chore) {
-        return DataRow(
-          cells: [
-            DataCell(Text(chore.name)),
-            DataCell(Text(chore.description)),
-            DataCell(Text(
-              // format the DateTime to 'YYYY‑MM‑DD'
-              chore.dateCreated.toLocal().toString().split(' ')[0],
-            )),
-            DataCell(Text(chore.completed ? '✅' : '❌')),
-            DataCell(
-              IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (_) => DeleteChorePopup(
-                      choreName: chore.name,
-                      onDelete: () => onDelete(chore.id!),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        );
-      }).toList(),
+      rows:
+          chores.map((chore) {
+            return DataRow(
+              cells: [
+                DataCell(Text(chore.name)),
+                DataCell(Text(chore.description)),
+                DataCell(
+                  Text(
+                    // format the DateTime to 'YYYY‑MM‑DD'
+                    chore.dateCreated.toLocal().toString().split(' ')[0],
+                  ),
+                ),
+                DataCell(Text(chore.completed ? '✅' : '❌')),
+                DataCell(
+                  IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder:
+                            (_) => DeleteChorePopup(
+                              choreName: chore.name,
+                              onDelete: () => onDelete(chore.id!),
+                            ),
+                      );
+                    },
+                  ),
+                ),
+                DataCell(
+                  IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EditChoreScreen(chore: chore),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          }).toList(),
     );
   }
 }
